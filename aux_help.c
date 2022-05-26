7df628ad00370bc8ca11c43dd14ad093aab5109e
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-05-27 02:32:28 +0300

    My commit

--- aux_help.c
+++ aux_help.c
@@ -51,9 +51,9 @@ void aux_help_general(void)
 char *help = "^-^ bash, version 1.0(1)-release\n";
 
 write(STDOUT_FILENO, help, _strlen(help));
-help = "These commands are defined internally.Type help to see the list";
+help = "These commands are defined internally.Type 'help' to see the list";
 write(STDOUT_FILENO, help, _strlen(help));
-help = "Type help name to find out more about the function name.\n\n ";
+help = "Type 'help name' to find out more about the function 'name'.\n\n ";
 write(STDOUT_FILENO, help, _strlen(help));
 help = " alias: alias [name=[string]]\n cd: cd [-L|[-P [-e]] [-@]] ";
 write(STDOUT_FILENO, help, _strlen(help));
